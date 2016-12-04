b6bd84d4d0ad5f9b4c0d72359fae5e8134ff3b5b
Borislav Nikolaev Ivanov <borislavnivanov@gmail.com>
2016-12-04 23:34:25 +0200

    fhd

---
+++ EnterNumbers/EnterNumbers.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace EnterNumbers
+{
+    class EnterNumbers
+    {
+        static void Main(string[] args)
+        {
+            int[] inputNumbers;
+
+            inputNumbers = GetNumberArray();
+            try
+            {
+                AreSorted(inputNumbers);
+                PrintArray(inputNumbers);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Exception");
+            }
+        }
+
+        private static void PrintArray(int[] inputNumbers)
+        {
+            Console.Write("1 < ");
+            for (int i = 0; i < inputNumbers.Length; i++)
+            {
+                Console.Write("{0} < ", inputNumbers[i]);
+            }
+            if (inputNumbers[inputNumbers.Length-1] != 100)
+            {
+                Console.Write("100");
+            }
+
+        }
+
+        private static void AreSorted(int[] inputNumbers)
+        {
+            for (int i = 0; i < inputNumbers.Length - 2; i++)
+            {
+                if (!(inputNumbers[i] < inputNumbers[i + 1]))
+                {
+                    throw new Exception("inputs are not sorted correctly");
+                }
+                if (inputNumbers[i] <= 1 || inputNumbers[i] >= 100)
+                {
+                    throw new IndexOutOfRangeException("input out of range");
+                }
+            }
+
+        }
+        private static int[] GetNumberArray()
+        {
+            int[] input = new int[10];
+            for (int i = 0; i < input.Length; i++)
+            {
+                input[i] = int.Parse(Console.ReadLine());
+            }
+            return input;
+        }
+    }
+}
---
+++ SquareRoot/SquareRoot.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+
+namespace SquareRoot
+{
+    class SquareRoot
+    {
+        static double NumberParse(string input)
+        {
+            double result;
+            result = double.Parse(input);
+            return result;
+        }
+        static double GetRoot(double input)
+        {
+            double result;
+            result = Math.Sqrt(input);
+            if (result.ToString() == "NaN")
+            {
+                throw new Exception ("resultNaN");
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            try
+            {
+                double number = NumberParse(input);
+                double result = GetRoot(number);
+                Console.WriteLine("{0:F3}", result);
+            }
+            catch
+            {
+                Console.WriteLine("Invalid number");
+            }
+            finally
+            {
+                Console.WriteLine("Good bye");
+            }
+        }
+    }
+}
